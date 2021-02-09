0f8c7f7b86b34d035e0ee91fbd218b482e2f504b
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2021-02-09 21:59:55 +1100

    add dotnet env vars

--- Infrastructure/Security/JwtGenerator.cs
+++ Infrastructure/Security/JwtGenerator.cs
@@ -5,12 +5,18 @@ using System.Security.Claims;
 using System.Text;
 using Application.Interfaces;
 using Domain;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Infrastructure.Security
 {
     public class JwtGenerator : IJwtGenerator
     {
+        private readonly SymmetricSecurityKey _key;
+        public  JwtGenerator(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]))
+        };
         public string CreateToken(AppUser user)
         {
             var claims = new List<Claim>
